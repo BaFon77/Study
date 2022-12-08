d9d4bd460158fbd26175a1613749938280709c4f
BaFon77 <antocha-spartak@yandex.ru>
2022-12-08 21:45:43 +0300

    new function manag file

--- ftp-server/server.py
+++ ftp-server/server.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import shutil
 
 '''
 pwd - показывает название рабочей директории
@@ -9,17 +10,19 @@ cat <filename> - отправляет содержимое файла
 
 dirname = os.path.join(os.getcwd(), 'docs')
 print(dirname)
+print(os.getcwd())
 
 
 def process(req):
     print('req = ', req)
+    print(req[:14] == 'copy_to_server')
 
     if req == 'pwd':
         return dirname
     elif req == 'ls':
         return '; '.join(os.listdir(dirname))
     elif req[:3] == 'cat':
-        path = os.path.join(os.getcwd(), 'docs', req[4::])
+        path = os.path.join(os.getcwd(), 'docs', req[4:])
         if os.path.exists(path):
             with open(path, 'r+') as file:
                 line = ''
@@ -29,8 +32,41 @@ def process(req):
         else:
             return 'Файла не существует'
 
+    elif req[:5] == 'mkdir':
+        path = os.path.join(os.getcwd(), 'docs', req[5:])
+        if not os.path.exists(path):
+            os.mkdir(path)
+            return 'Папка успешно создана'
+        return 'Папка скорее всего уже создана'
 
+    elif req[:5] == 'rmdir':
+        path = os.path.join(os.getcwd(), 'docs', req[5:])
+        if os.path.exists(path):
+            os.rmdir(path)
+            return 'Папка успешно удалена'
+        return 'Папка скорее всего уже удалена'
 
+    elif req[:6] == 'rename':
+        req = req.split()
+        path1 = os.path.join(os.getcwd(), 'docs', req[1])
+        path2 = os.path.join(os.getcwd(), 'docs', req[2])
+        if os.path.exists(path1):
+            os.rename(path1, path2)
+            return 'Файл успешно переименован'
+        return 'Такого файла не существует'
+
+    elif req[:14] == 'copy_to_server':
+        print(1)
+        req = req.split()
+        path = os.path.join(os.getcwd(), 'docs', req[1])
+        shutil.copyfile(req[2], path)
+        return 'Файл успешно скопирован'
+
+    elif req[:16] == 'copy_from_server':
+        req = req.split()
+        path = os.path.join(os.getcwd(), 'docs', req[1])
+        shutil.copyfile(path, req[2])
+        return 'Файл успешно скопирован'
 
     return 'bad request'
 
